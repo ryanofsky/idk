122cee8eb0a1699c92f25d80ef49c1004697f186
Russell Yanofsky <russ@yanofsky.org>
2016-10-11 20:56:08 -0400

    Add Idk smart contract & test.
    Not yet called from UI.

---
+++ contracts/Idk.sol
@@ -0,0 +1,65 @@
+contract Idk {
+  struct Pledge {
+    address from;
+    uint deadline;
+    uint amount;
+    bool disbursed;
+  }
+
+  mapping (address => uint) balances;
+
+  mapping (uint => Pledge) pledges;  // Map from sqlite pledgeId to pledge info.
+
+  function deposit() {
+      balances[msg.sender] += msg.value;
+  }
+
+  function withdraw(uint amount) {
+      if (balances[msg.sender] < amount || amount == 0)
+          throw;
+      balances[msg.sender] -= amount;
+      if (!msg.sender.send(amount)) throw;
+  }
+
+  function pledge(uint pledgeId, uint amount, uint deadline) {
+    if (balances[msg.sender] < amount || amount == 0)
+        throw;
+    if (pledges[pledgeId].amount != 0)
+        throw;
+    balances[msg.sender] -= amount;
+    pledges[pledgeId] = Pledge({from: msg.sender, deadline: deadline, amount: amount, disbursed: false});
+  }
+
+  function disbursePledge(uint pledgeId, address[] payTo, uint[] payAmount) {
+    // Do checks.
+    Pledge pledge = pledges[pledgeId];
+    if (now < pledge.deadline)
+        throw;
+    if (pledge.from != msg.sender)
+        throw;
+    if (pledge.disbursed)
+        throw;
+    if (payTo.length != payAmount.length)
+        throw;
+    uint newBalance = balances[pledge.from] + pledge.amount;
+    for (uint i = 0; i < payTo.length; i++) {
+        if (payAmount[i] > newBalance) // Can't exceed available funds.
+            throw;
+        if (payTo[i] == pledge.from) // Sending to self would break logic below.
+            throw;
+        newBalance -= payAmount[i];
+    }
+
+    // Update state.
+    pledge.disbursed = true;
+    pledges[pledgeId] = pledge;
+    for (i = 0; i < payTo.length; i++) {
+        balances[payTo[i]] += payAmount[i];
+    }
+    balances[pledge.from] = newBalance;
+  }
+
+	function getBalance(address addr) returns(uint) {
+		return balances[addr];
+	}
+}
---
+++ test/idk.js
@@ -0,0 +1,25 @@
+contract('Idk', function(accounts) {
+  it("should pledge & disburse", function() {
+    var pledgeId = 12345;
+    var deadline = 0;
+    var idk = Idk.deployed();
+    return idk.deposit({from: accounts[0], value: 10000}).then(function() {
+      return idk.getBalance.call(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance, 10000);
+      return idk.pledge(pledgeId, 7000, 0, {from: accounts[0]});
+    }).then(function() {
+      return idk.getBalance.call(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance, 3000);
+      return idk.disbursePledge(pledgeId, [accounts[1]], [1000], {from: accounts[0]});
+    }).then(function() {
+      return idk.getBalance.call(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance, 9000);
+      return idk.getBalance.call(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance, 1000);
+    });
+  });
+});
